7ef1903b2235b91061800500db6cb74d6f703a26
kao98 <kao98@kaonet-fr.net>
2014-04-07 19:15:06 +0200

    Just added some comments.

--- test/unit/logger/Logger.js
+++ test/unit/logger/Logger.js
@@ -1,13 +1,37 @@
+/**
+ * Tests for the base class Logger.
+ * Those tests may be consider as functional tests as
+ * the tests for the 'base' class shall represent
+ * various usage of the JSLogger using the default
+ * writer.
+ */
 LoggerTest = TestCase("LoggerTest");
 
+/**
+ * We setUp our tests.
+ */
 LoggerTest.prototype.setUp = function () {
+
+    // The default writer for the JSLogger class should use the
+    // 'console' global object. The easiest way to test the
+    // logger is then to spy the 'console' object to check
+    // if it is properly called when it should be.
+
     sinon.spy(console, "log");  //Spy the console.log method
 };
 
+/**
+ * We release the spies we set in the setUp method.
+ */
 LoggerTest.prototype.tearDown = function () {
     console.log.restore(); //Unwrapp the spy
 };
 
+/**
+ * Simplest test / use we could do of the Logger:
+ * KNT.LOGGER.log(message).
+ * It should simply call console.log with the given message.
+ */
 LoggerTest.prototype.testLogger = function () {
     
     //Given
@@ -21,6 +45,13 @@ LoggerTest.prototype.testLogger = function () {
     
 };
 
+/**
+ * Another really simple usage of the Logger:
+ * KNT.LOGGER.log(message, context) with a context.
+ * It should simply call the console.log method
+ * with the given message after keys have been
+ * processed (interpolated) using the context array.
+ */
 LoggerTest.prototype.testLoggerInterpolation = function () {
     
     //Given
